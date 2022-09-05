fa9388704c8dbf0a247789eb22f8bc63662067a2
Joao-prrrr <joao.prrrr@eduge.ch>
2022-09-05 13:14:12 +0200

    Adding public folders

---
+++ app.js
@@ -0,0 +1,10 @@
+/// Reshoes project
+/// Author : Joao Araribá, Louis Saulnier and Bastien Schneider
+/// Date : 05.09.2022 v1
+
+const { Router } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.use(router)
