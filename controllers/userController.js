706e507f6e187a1f850266e317d226ae043c6c19
Joao-prrrr <joao.prrrr@eduge.ch>
2022-09-12 15:33:13 +0200

    Coding the gets methodes to change pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
 const User = require('../models/User')
 
-
+// Render the home page
 exports.home = function(req, res) {
     res.render('index')
 }
 
+// Called to render the ejs page
 exports.pageMarque = function(req, res) {
-    res.render('page-jordan')
+    // get the path to know which page render
+    let file = req.path
+    file = file.slice(1)
+    res.render(file)
 }
--- router.js
+++ router.js
@@ -1,13 +1,21 @@
+// Router file who controls the get methods
+
 const express = require('express')
 const router = express.Router()
 
 const userController = require('./controllers/userController')
 
+// go to home page
 router.get('/', userController.home)
-router.get('/page-jordan', userController.pageJordan)
-router.get('/page-yeezy', userController.pageYeezy)
-router.get('/page-newBalance', userController.pageNewBalance)
-router.get('/page-nike', userController.pageNike)
-router.get('/page-luxe', userController.pageLuxe)
+// go to jordan page
+router.get('/page-jordan', userController.pageMarque)
+// go to Yeezy page
+router.get('/page-yeezy', userController.pageMarque)
+// go to New Balance page
+router.get('/page-newBalance', userController.pageMarque)
+// go to Nike page
+router.get('/page-nike', userController.pageMarque)
+// go to Luxe page
+router.get('/page-luxe', userController.pageMarque)
 
 module.exports = router
