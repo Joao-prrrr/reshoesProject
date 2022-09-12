6f101339a685c30924cede6f5c7c254c3c8208cb
Joao-prrrr <joao.prrrr@eduge.ch>
2022-09-12 15:15:03 +0200

    Adding .get() in router

--- router.js
+++ router.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 
 router.get('/', userController.home)
-router.get('/page-jordan', userController.pageMarque)
+router.get('/page-jordan', userController.pageJordan)
+router.get('/page-yeezy', userController.pageYeezy)
+router.get('/page-newBalance', userController.pageNewBalance)
+router.get('/page-nike', userController.pageNike)
+router.get('/page-luxe', userController.pageLuxe)
 
 module.exports = router
