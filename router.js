a86777e2523a1b10b63603789a409ab68c6c42fd
Joao-prrrr <joao.prrrr@eduge.ch>
2022-09-12 13:54:02 +0200

    Adding img folder and some .get()

--- router.js
+++ router.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 
 router.get('/', userController.home)
+router.get('/page-jordan')
 
 module.exports = router
