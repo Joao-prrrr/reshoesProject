d36d58125f25cf2413feb8efd37073cc36833ef5
Louis-Saulnier <louis.slnr@eduge.ch>
2022-12-05 14:10:59 +0100

    try

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 
 const MongoStore = require('connect-mongo')
-const router= require('./router')
+const router = require('./router')
 const express = require('express')
 const session = require('express-session')
 const flash = require('connect-flash')
@@ -13,19 +13,19 @@ const app = express()
 
 const sessionOption = session({
     secret: 'reshoesApp',
-    store: MongoStore.create({client: require('./db')}),
+    store: MongoStore.create({ client: require('./db') }),
     resave: false,
     saveUninitialized: false,
     // 1000*60*60*24 == days
-    cookie: { maxAge: 1000*60*60*24, httpOnly: true}
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }
 })
 
 app.use(sessionOption)
 app.use(flash())
-// app.use(express.cookieParser('keyboard cat'));
-// app.use(express.session({ cookie: { maxAge: 60000 }}));
-// app.use(flash());
-app.use(express.urlencoded({ extended:false })) // for form data
+    // app.use(express.cookieParser('keyboard cat'));
+    // app.use(express.session({ cookie: { maxAge: 60000 }}));
+    // app.use(flash());
+app.use(express.urlencoded({ extended: false })) // for form data
 app.use(express.json()) // for json
 
 app.use(express.static('public'))
@@ -33,23 +33,23 @@ app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 
-app.use(function(req, res , next) {
+app.use(function(req, res, next) {
     //make all error and succes flash messages available
     res.locals.errors = req.flash('errors')
     res.locals.success = req.flash('success')
     res.locals.regErrors = req.flash('regErrors')
     res.locals.regSuccess = req.flash('regSuccess')
-    // make current user id avilable on the req object
-    if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+        // make current user id avilable on the req object
+    if (req.session.user) { req.visitorId = req.session.user._id } else { req.visitorId = 0 }
     // make user sassion data available from within view templates
     res.locals.user = req.session.user
-    // res.locals.currentShoes = req.session.currentShoes;
+        // res.locals.currentShoes = req.session.currentShoes;
 
-    if(req.path != "/") {
+    if (req.path != "/") {
         let file = req.path
-        // file = file.slice(6)
-        // let firstLetter = file.charAt(0).toUpperCase()
-        // file = firstLetter + file.slice(1)
+            // file = file.slice(6)
+            // let firstLetter = file.charAt(0).toUpperCase()
+            // file = firstLetter + file.slice(1)
         file = file.slice(file.indexOf('/') + 1, file.indexOf('/', 1));
         res.locals.marque = file
     } else {
